80a4e8971058c61bdeecde89e6c97247ed40ae98
smag-dev <sergiomagomes@gmail.com>
2023-02-12 20:10:09 +0000

    conexão ao mongo

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import dotenv from 'dotenv';
 import productRouter from "./routers/productRouter";
+import mongoose from "mongoose";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const MONGO_URI: string = String(process.env.MONGO_URI);
 const app = express();
 app.use(express.json())
 
 const startApp = async () => {
     try {
+      mongoose.set("strictQuery", true);
+      await mongoose.connect(MONGO_URI);
+      console.log("Successefully connected to db");
       app.listen(PORT, () => {
         console.log(`Server started on PORT: ${PORT}`);
       });
