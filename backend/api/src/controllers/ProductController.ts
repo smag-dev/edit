8d873bc78d50c740acdb90145fb39f62ed5948ca
smag-dev <sergiomagomes@gmail.com>
2023-02-12 22:24:32 +0000

    apanhar os erros no Productcontroller caso dê erro na execução dos pedidos

--- backend/api/src/controllers/ProductController.ts
+++ backend/api/src/controllers/ProductController.ts
@@ -3,35 +3,41 @@ import ProductService from "../services/ProductService";
 
 class ProductController {
     create(req : Request, res : Response) {
-        //console.log(req.body)
-        //console.log(req.params);
-        const product = ProductService.create(req.body);
-        res.status(200).json('primeiro pedido post products')
+        try {
+            const product = ProductService.create(req.body);
+            res.status(201).send(product)
+        }catch (error) {
+            res.status(500).send({ message: "Falhou a criação do produto.", error });
+        }
     }
 
     getAll(req : Request, res : Response)  {
-        //console.log(req.params);
-        //console.log(req.body)
-        const products = ProductService.getAll();
-        res.status(200).json('primeiro pedido get products')
+        try{
+            const products = ProductService.getAll();
+            res.status(201).send(products)
+        }catch(error){
+            res.status(500).send({ message: "Falhou na obtenção de produtos.", error });      
+        }
     }
 
     getOne(req : Request, res : Response)  {
-        const { id } = req.params;
-        //console.log(id);
-        //console.log(req.params);
-        //console.log(req.body)
-        const product = ProductService.getOne(id);
-        res.status(200).json('primeiro pedido get products')
+        try{
+            const { id } = req.params;
+            const product = ProductService.getOne(id);
+            res.status(200).send(product)
+        }catch(error){
+            res.status(500).send({ message: "Falhou na obtenção do produto.", error })
+        }
     }
 
     update(req : Request, res : Response) {
-        const { id } = req.params;
-        //console.log(id);
-        //console.log(req.params);
-        //console.log(req.body)
-        const product = ProductService.update(id, req.body);
-        res.status(200).json('primeiro pedido put products')
+        try{
+            const { id } = req.params;
+            const product = ProductService.update(id, req.body);
+            res.status(200).send(product)
+        }catch(error){
+            res.status(500).send({ message: "Falhou na atualização do produto.", error })
+        }
     }
 
     delete(req : Request, res : Response){
