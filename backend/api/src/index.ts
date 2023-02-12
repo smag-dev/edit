e7917afa4005ab13d1124e8ec4e4baee447b22f4
smag-dev <sergiomagomes@gmail.com>
2023-02-12 17:57:13 +0000

    ligação ao ficheiro.env, conexão ao servidor, primeiros pedidos

--- backend/api/src/index.ts
+++ backend/api/src/index.ts
@@ -1,1 +1,46 @@
-console.log("teste");
+import express from "express";
+import dotenv from 'dotenv';
+import { Request, Response } from "express";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json())
+
+const startApp = async () => {
+    try {
+      app.listen(PORT, () => {
+        console.log(`Server started on PORT: ${PORT}`);
+      });
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error.message);
+      }
+    }
+};
+
+startApp();
+
+app.get('/products', (req : Request, res : Response) => {
+    console.log("primeiro pedido get products");
+    console.log(req.query)
+    res.status(200).json('primeiro pedido get products')
+})
+
+app.post('/products', (req : Request, res : Response) => {
+    console.log("primeiro pedido post products");
+    console.log(req.query)
+    res.status(200).json('primeiro pedido post products')
+})
+
+app.put('/products/', (req : Request, res : Response) => {
+    console.log("primeiro pedido put products");
+    console.log(req.query)
+    res.status(200).json('primeiro pedido put products')
+})
+
+app.delete('/products/', (req : Request, res : Response) => {
+    console.log("primeiro pedido delete products");
+    console.log(req.query)
+    res.status(200).json('primeiro pedido delete products')
+})
