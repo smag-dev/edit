8ae3df90dcd05af5fbfe9780476d4a3824566ab3
smag-dev <sergiomagomes@gmail.com>
2023-02-19 02:24:25 +0000

    correção de erro quando se tenta fazer login com email errado

--- backend/api/src/controllers/UserController.ts
+++ backend/api/src/controllers/UserController.ts
@@ -28,11 +28,11 @@ class UserController {
       /* apanhar erro das  validações */
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        res.status(400).json({ "Validation errors.": errors.array() });
+        next(res.status(400).json({ "Validation errors.": errors.array() }));
+      } else {
+        const user = await UserService.login(req.body);
+        res.status(200).json(user);
       }
-      const user = await UserService.login(req.body);
-
-      res.status(200).json(user);
     } catch (error: unknown) {
       if (error instanceof Error) {
         res.status(500).json({ message: "falhou login", error: error.message });
